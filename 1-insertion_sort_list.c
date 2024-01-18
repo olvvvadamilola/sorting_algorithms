97a9553ac8b774630348b0b96ec92d26440ae3d9
olvvvadamilola <destric3@gmail.com>
2024-01-18 14:01:38 +0100

    insertion and selection sorting

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "sort.h"
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *c_node, *temp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	current = (*list)->next;
+
+	while (current != NULL)
+	{
+		c_node = current;
+
+		while (c_node->prev != NULL && c_node->n < c_node->prev->n)
+		{
+			/* Swap nodes */
+			temp = c_node->prev;
+			temp->next = c_node->next;
+			if (c_node->next != NULL)
+				c_node->next->prev = temp;
+			c_node->prev = temp->prev;
+			c_node->next = temp;
+			temp->prev = c_node;
+
+			if (c_node->prev != NULL)
+				c_node->prev->next = c_node;
+			else
+				*list = c_node;
+
+			/* Print the list after each swap */
+			print_list(*list);
+		}
+
+		current = current->next;
+	}
+}
